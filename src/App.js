7c474b17ae752d6d460458eef76b75fcb42ef428
MarkRab <chunmu.yao@gmail.com>
2024-04-15 18:42:11 +1000

    Rev_1.3 Form interactive added

--- src/App.js
+++ src/App.js
@@ -51,17 +51,20 @@ function Counter() {
 function App() {
   // Define state variable
   const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
 
   return (
     <>
       <Header showForm={showForm} setShowForm={setShowForm} />
 
       {/* 2. use state variable */}
-      {showForm ? <NewFactForm /> : null}
+      {showForm ? (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowForm} />
+      ) : null}
 
       <main className="main">
         <CategoryFilter />
-        <FactList />
+        <FactList facts={facts} />
       </main>
     </>
   );
@@ -98,21 +101,58 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-function NewFactForm() {
+function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
   const [source, setSource] = useState("");
   const [category, setCategory] = useState("");
   const textLength = text.length;
+
+  function isValidHttpUrl(string) {
+    let url;
+
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
 
   function handleSubmit(e) {
     // Prevent browser reload
     e.preventDefault();
     console.log(text, source, category);
     // 2. Check if the data is valid. If so, create a new fact.
+    if (text && isValidHttpUrl(source) && category && textLength <= 200) {
+      // 3. Create a new fact object
+      const newFact = {
+        id: Math.round(Math.random * 10000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
 
-    // 3. Create a new fact object
-
-    // 4. Add the new fact to the UI: add the fact to state
+      // 4. Add the new fact to the UI: add the fact to state
+      setFacts((facts) => [newFact, ...facts]);
+      // 5. Reset input fields
+      setText("");
+      setSource("");
+      setCategory("");
+      // 6. Close the form
+      setShowForm(false);
+      // 7. Remind if url is not correct
+    } else if (!isValidHttpUrl(source)) {
+      {
+        {
+          alert("Source is not valid url");
+        }
+      }
+    }
   }
 
   return (
@@ -165,9 +205,8 @@ function CategoryFilter() {
   );
 }
 
-function FactList() {
+function FactList({ facts }) {
   //  TEMPORARY
-  const facts = initialFacts;
 
   return (
     <section>
